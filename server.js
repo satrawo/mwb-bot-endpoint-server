393a603092d8fe577f9b5ea8eeb23242bcbd8678
Sorawat Tanthikun <st.sorawat38@gmail.com>
2018-01-24 16:52:18 +0700

    edit port number

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function() {
-    console.log('Chatfuel Bot-Server listening on port 3000...');
+app.listen(80, function() {
+    console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
 app.get('/*', function(req, res) {
