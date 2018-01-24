26005ed8a0db78f5f1a46b9ed0ece90d8bcd4f38
Sorawat Tanthikun <st.sorawat38@gmail.com>
2018-01-24 17:06:28 +0700

    add http

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 http.listen(process.env.PORT || 3000, function() {
     console.log('listening on', http.address().port);
