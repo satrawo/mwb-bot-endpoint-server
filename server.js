553f6f6540f400ea9f766555698c8151afa51ea2
Sorawat Tanthikun <st.sorawat38@gmail.com>
2018-01-24 16:43:47 +0700

    first demo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.listen(80, function() {
-    console.log('Chatfuel Bot-Server listening on port 80...');
+app.listen(3000, function() {
+    console.log('Chatfuel Bot-Server listening on port 3000...');
 });
 
 app.get('/*', function(req, res) {
