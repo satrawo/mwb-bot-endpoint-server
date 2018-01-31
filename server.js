0126c0a6bbbe8340e75743498a1900816a1e8ade
Sorawat Tanthikun <st.sorawat38@gmail.com>
2018-01-31 14:36:06 +0700

    accept parameter

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ http.listen(process.env.PORT || 3000, function() {
 
 app.get('/*', function(req, res) {
     var jsonResponse = [];
-    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
-    res.send(jsonResponse);
+    var user_fb_id = req.param('fb_id');
+    var firstScore = req.param('attribute1');
+    var secondScore = req.param('attribute2');
+    var thridScore = req.param('attribute3');
+    var fourthScore = req.param('attribute4');
+    var fifthScore = req.param('attribute5');
+    //Total Score
+    var totalScore = firstScore + secondScore + thridScore + fourthScore + fifthScore;
+    
+    // jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    res.json({ "result": totalScore });
+    //res.send(jsonResponse);
 });
 
